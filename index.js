0620d2e72f8a1c45902e59cdb68baf67b02c3a56
Genesis (Oliver) G <107947533+theurbanspectacle@users.noreply.github.com>
2022-12-14 18:11:48 -0800

    finished

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const fs = require("fs");
-
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
+const uniqid = require('uniqid');
+
+const app = express(); 
 const port = 3000;
 
+app.use(bodyParser.json());
 
 app.get('/api/notes', (req, res) => {
     fs.readFile("./db/db.json", (err, data) => {
@@ -22,7 +25,54 @@ app.get('/notes', (req, res) => {
     res.sendFile('./public/notes.html', { root: __dirname });
 });
 
+app.post('/api/notes', (req, res) => {
+    const newNote =  {
+        title: req.body.title,
+        text: req.body.text,
+        id: uniqid()
+    }; 
+    fs.readFile("./db/db.json" , (err, data) => {
+        if (err) {
+            console.error("Could not read file", err);
+            res.status(500).send({error: 'failed!'}); 
+            return;
+        }
+        const notes = JSON.parse(data.toString());
+        notes.push(newNote);
 
+        fs.writeFile("./db/db.json", JSON.stringify(notes, undefined, 2), (err, data) => {
+            if (err) {
+                console.error("Could not read file", err);
+                res.status(500).send({error: 'failed!'}); 
+                return;
+            }
+            res.json(newNote);
+        });
+    });
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    fs.readFile("./db/db.json" , (err, data) => {
+        if (err) {
+            console.error("Could not read file", err);
+            res.status(500).send({error: 'failed!'}); 
+            return;
+        }
+        let notes = JSON.parse(data.toString());
+        notes = notes.filter(item => {
+            return item.id !== req.params.id;
+        });
+
+        fs.writeFile("./db/db.json", JSON.stringify(notes, undefined, 2), (err, data) => {
+            if (err) {
+                console.error("Could not read file", err);
+                res.status(500).send({error: 'failed!'}); 
+                return;
+            }
+            res.send();
+        });
+    });
+});
 
 
 app.listen(port, () => {
